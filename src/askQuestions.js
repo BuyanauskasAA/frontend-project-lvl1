b05f6a1d9056b11384336d3acadd57ea8311bea9
BuyanauskasAA <Buyanauskas@gmail.com>
2021-03-22 23:56:12 +0300

    create src/askQuestions.js

---
+++ src/askQuestions.js
@@ -0,0 +1,26 @@
+import readlineSync from 'readline-sync';
+import getRandomInt from './getRandomInt.js';
+import isEven from './isEven.js';
+
+export default (questionsCount) => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+  for (let i = 0; i < questionsCount; i += 1) {
+    const randomInt = getRandomInt(1, 100);
+
+    console.log(`Question: ${randomInt}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (isEven(randomInt) !== answer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer is '${isEven(randomInt)}'.\nLet's try again, ${name}!`);
+      break;
+    }
+    console.log('Correct!');
+
+    if (i === questionsCount - 1) {
+      console.log(`Congratulations, ${name}!`);
+    }
+  }
+};
