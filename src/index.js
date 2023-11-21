482b123e3e9122dd5e3fb46b5db0640ec2262641
BuyanauskasAA <Buyanauskas@gmail.com>
2023-11-21 23:01:47 +0600

    fix: add description

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default (rulesOfTheGame, getConditions) => {
+  console.log(rulesOfTheGame);
   console.log('Welcome to the Brain Games!');
 
   const name = readlineSync.question('May I have your name? ');
